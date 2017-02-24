343d7bb3ce9218b925c4421a6885eb6244d23eda
Will Currie <will@currie.id.au>
2017-02-25 10:42:54 +1100

    Report spans in logs but no zipkin reporting yet

---
+++ README.md
@@ -0,0 +1,3 @@
+## Resources
+
+https://spring.io/blog/2016/02/15/distributed-tracing-with-spring-cloud-sleuth-and-spring-cloud-zipkin
--- kitchenhand/src/main/java/com/example/kitchenhand/controller/SandwichController.java
+++ kitchenhand/src/main/java/com/example/kitchenhand/controller/SandwichController.java
@@ -2,6 +2,8 @@ package com.example.kitchenhand.controller;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +15,11 @@ import java.util.UUID;
 @RequestMapping("/sandwiches")
 public class SandwichController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(SandwichController.class);
+
     @PostMapping(produces = "application/json")
     public Map<String, Object> make() {
+        LOG.info("Making a sandwich");
         return new ImmutableMap.Builder<String, Object>()
                 .put("id", UUID.randomUUID())
                 .put("contents", ImmutableList.builder().build())
