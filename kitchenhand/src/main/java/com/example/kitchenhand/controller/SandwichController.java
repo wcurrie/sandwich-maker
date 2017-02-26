e54c2b260c634052fed196d2050e32b7b257e339
Will Currie <will@currie.id.au>
2017-02-26 13:25:40 +1100

    Add turbo sandwich mode using rxjava
    Loses zipkin tracing.

--- kitchenhand/src/main/java/com/example/kitchenhand/controller/SandwichController.java
+++ kitchenhand/src/main/java/com/example/kitchenhand/controller/SandwichController.java
@@ -2,6 +2,8 @@ package com.example.kitchenhand.controller;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import io.reactivex.Single;
+import io.reactivex.schedulers.Schedulers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.UUID;
 
@@ -42,12 +45,39 @@ public class SandwichController {
         LOG.info("Collecting ham");
         Map ham = restTemplate.getForObject(findInstance("butcher").resolve("/ham?slices=2"), Map.class);
 
+        return makeSandwich(Arrays.asList(bread, ham));
+    }
+
+    @PostMapping(produces = "application/json", params = "turbo=true")
+    public Map<String, Object> makeQuickly() {
+        LOG.info("Getting a candle to work by");
+        Map candle = restTemplate.getForObject(findInstance("candlestickmaker").resolve("/candles"), Map.class);
+        LOG.info("Lighting " + candle.get("id"));
+
+        // probably a terrible use of rx-java
+
+        LOG.info("Gathering ingredients");
+
+        Single<Map> bread = Single.fromCallable(() -> {
+            LOG.info("Collecting bread");
+            return restTemplate.getForObject(findInstance("baker").resolve("/loaves"), Map.class);
+        }).subscribeOn(Schedulers.io());
+
+        Single<Map> ham = Single.fromCallable(() -> {
+            LOG.info("Collecting ham");
+            return restTemplate.getForObject(findInstance("butcher").resolve("/ham?slices=2"), Map.class);
+        }).subscribeOn(Schedulers.io());
+
+        Iterable<Map> ingredients = Single.merge(bread, ham).blockingIterable();
+        return makeSandwich(ingredients);
+    }
+
+    private Map<String, Object> makeSandwich(Iterable<Map> ingredients) {
         LOG.info("Making a sandwich");
         return new ImmutableMap.Builder<String, Object>()
                 .put("id", UUID.randomUUID())
                 .put("contents", ImmutableList.builder()
-                        .add(bread)
-                        .add(ham)
+                        .addAll(ingredients)
                         .build())
                 .build();
     }
